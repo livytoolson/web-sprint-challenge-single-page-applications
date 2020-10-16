d1cb1dcb79c42958211827b81e83e05079269303
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-16 09:59:21 -0600

    set slices of state, created helper functions, created route, created link in app.js

---
+++ src/PizzaForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function PizzaForm(props) {
+    const {  } = props;
+
+    return (
+        <form>
+            {/* RENDER VALIDATION ERRORS */}
+
+            {/* NAME TEXT INPUT */}
+
+            {/* PIZZA SIZE DROPDOWN */}
+
+            {/* TOPPINGS CHECKLIST */}
+
+            {/* SPECIAL ORDER INSTRUCTIONS */}
+
+            {/* ORDER BUTTON */}
+            <button>Submit Order</button>
+        </form>
+    )
+}
