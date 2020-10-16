a19a44896c13587e9e3708e1974e7a6e22983730
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-16 11:50:45 -0600

    started writing tests

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -1,6 +1,26 @@
 describe('Lambda Eats', () => {
 
     beforeEach(() => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/pizzaform')
+    })
+
+    const nameInput = () => cy.get('input[name="name"]')
+    const specialInput = () => cy.get('input[name="specialInstructions"]')
+    const submitBtn = () => cy.get('#submitBtn')
+    
+    it('test that you can add text to the name input box', () => {
+        nameInput().type('Olivia')
+        nameInput().should('have.value', 'Olivia')
+    })
+
+    it('test that you can select multiple toppings', () => {
+
+    })
+
+    it('test that you can submit the form', () => {
+        submitBtn().should('exist')
+        nameInput().type('Olivia')
+        specialInput().type('Gluten Free')
+        submitBtn().click()
     })
 })
