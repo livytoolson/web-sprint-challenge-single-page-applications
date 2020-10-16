661bb8d0173d637b223df291b43a44c3c1f70825
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-16 09:14:34 -0600

    created inital values of form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import PizzaForm from './PizzaForm';
+import schema from './formSchema';
+import axios from 'axios';
+import * as yup from 'yup';
 
 const App = () => {
+
+  // INITIAL STATE OF THE FORM
+  const initialFormValues = {
+    // TEXT INPUT
+    name: '',
+    speacialInstructions: '',
+    // DROPDOWN
+    pizzaSize: '',
+    // CHECKBOXES
+    cheese: false,
+    pepperoni: false,
+    ham: false,
+    sausage: false,
+  }
   return (
     <>
       <h1>Lambda Eats</h1>
